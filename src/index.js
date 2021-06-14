d07f3154c96f4fcaff3413fa077f3bf817e095f7
Nguyen Ngoc Bang <bang270197@gmail.com>
2021-06-14 09:29:04 +0700

    add cors npm

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
+var cors = require("cors");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const db = require("./config/connect");
@@ -17,6 +18,7 @@ session.configSession(app);
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+app.use(cors());
 route(app);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
