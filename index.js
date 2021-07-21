86fbcf116dc4241d9406e98c9542a2f34e8c11a8
Nguyen Ngoc Bang <bang270197@gmail.com>
2021-07-21 17:10:08 +0700

    make it better

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const port = process.env.PORT || 8080;
 var cors = require("cors");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const db = require("./config/connect");
-const route = require("./routes/index.routes");
-const session = require("./config/session");
-app.use(
-    "/static",
-    express.static(path.resolve(__dirname, "../public/uploads"))
-);
+const db = require("./src/config/connect");
+const route = require("./src/routes/index.routes");
+const session = require("./src/config/session");
+// app.use(
+//     "/static",
+//     express.static(path.resolve(__dirname, "../public/uploads"))
+// );
 db.connectDB().catch(console.log);
 session.configSession(app);
 app.get("/", (req, res) => {
